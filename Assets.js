bcb39a41e4c79ebb66f29c5549dc39292f8643a7
Kathy Cui <kcui2014@gmail.com>
2023-11-30 04:41:47 -0600

    Implement range function

--- Assets.js
+++ Assets.js
@@ -1,3 +1,18 @@
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+function range(array) {
+    minValue = Infinity;
+    maxValue = -Infinity;
+    for (item of array) {
+        // Find minimum value
+        if (item < minValue)
+            minValue = item;
+                
+        // Find maximum value
+        if (item > maxValue)
+            maxValue = item;
+    return maxValue - minValue;
+}
+}
